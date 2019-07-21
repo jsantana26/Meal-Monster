23eb9b22d2895ed69023a9934764661c4cfe15a9
Jeffrey <jeffreysantana26@gmail.com>
2019-07-21 18:27:35 -0500

    Routes for authentication

---
+++ auth/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+const User = require('../db/user');
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'success'
+  });
+});
+
+function validUser(user) {
+  const validEmail = typeof user.email == 'string' && user.email.trim() != '';
+  const validPassword = typeof user.password == 'string' && user.password.trim() != '' && user.password.trim().length >= 6;
+
+  return validEmail && validPassword;
+}
+
+router.post('/signup', (req, res, next) => {
+  //Make sure username and password are valid
+  if (validUser(req.body)) {
+    //Check if email is already in use
+    User.getOneByEmail(req.body.email)
+      .then(user => {
+        if (!user) {
+          //Email is not in use
+          bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+              const user = {
+                name: req.body.name,
+                email: req.body.email,
+                password: hash,
+                username: req.body.username,
+                dateCreated: new Date()
+              }
+              User.create(user)
+                .then(id => {
+                  res.json({ id: id, message: 'success' });
+                });
+
+            });
+        } else {
+          //Email is already in use
+          next(new Error('Email in use'));
+        }
+      });
+  } else {
+    next(new Error('Invalid user'));
+  }
+});
+
+router.post('/login', (req, res, next) => {
+  if (validUser(req.body)) {
+    //Check db for user
+    User.getOneByEmail(req.body.email)
+      .then(user => {
+        console.log('user', user);
+        //If user email was found in db
+        if (user) {
+          //Compare password
+          bcrypt.compare(req.body.password, user.password)
+            .then(result => {
+              //setting the set 'set-cookie' header
+              res.cookie('user_id', user.id, {
+                httpOnly: true,
+                //signed: true, WHEN IN PRODUCTION
+                secure: true
+              });
+              if (result) {
+                res.json({ result, message: 'Logged In!' });
+              } else {
+                next(new Error('Invalid login'));
+              }
+
+            })
+
+        } else {
+          next(new Error('Invalid login'));
+        }
+      });
+  } else {
+    next(new Error('Invalid login'));
+  }
+});
+
+module.exports = router;
