dffb37e542e2405b258d58eba6adca6fc09294f7
Jeffrey <jeffreysantana26@gmail.com>
2019-07-21 18:26:09 -0500

    Installed packages and set up middle ware Cors, bodyparser, doenv, cookieparser,knex

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 const app = express();
 
 const port = 3000;
 
+//Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
 //Import Routes
 const users = require('./routes/users');
+const auth = require('./auth/index');
 
 //Route Middleware
 app.use('/users', users);
+app.use('/auth', auth);
 
+//Home page
 app.get('/', (req, res) => {
   res.send('Index');
 });
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({ message: err.message, error: req.app.get('env') === 'development' ? err : {} })
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
