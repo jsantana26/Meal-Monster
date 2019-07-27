c2a3912d1d5d12fc2d04f3f1af76242f8df687a9
Jeffrey <jeffreysantana26@gmail.com>
2019-07-27 07:10:33 -0500

    Add register route

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { RegisterComponent } from './components/account/register/register.component';
 import { AuthService } from './services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages'
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,13 +12,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/account/login/login.component';
-import { RegisterComponent } from './components/account/register/register.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
 
-  { path: 'account/login', component: LoginComponent }
-];
+  { path: 'account/login', component: LoginComponent },
+  { path: 'account/register', component: RegisterComponent }
+]
 
 @NgModule({
   declarations: [
